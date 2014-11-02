d51016af71d37e6d5ea3c6e846bfaf8c383a5a81
KuoE0 <kuoe0.tw@gmail.com>
2014-11-03 07:03:54 +0800

    quote script path

--- install.sh
+++ install.sh
@@ -33,8 +33,8 @@ if [ -f ~/.vimrc ] || [ -h ~/.vimrc ]; then
 fi
 
 # re-link them with current file
-ln -s $SCRIPTPATH ~/.vim
-ln -s $SCRIPTPATH/vimrc ~/.vimrc
+ln -s "$SCRIPTPATH" ~/.vim
+ln -s "$SCRIPTPATH/vimrc" ~/.vimrc
 
 # create undo directory
 if [ ! -d ~/.vim/undo ]; then
